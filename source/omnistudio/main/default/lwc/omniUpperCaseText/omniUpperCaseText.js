4abff88b51c6ce777b8d957e60400fc57b4ff759
Apex Force Pty Ltd <ContactUsMelbourne@gmail.com>
2024-06-10 08:29:42 +1000

    Replace Rego Text Input with LWC omniUpperCaseText

---
+++ source/omnistudio/main/default/lwc/omniUpperCaseText/omniUpperCaseText.js
@@ -0,0 +1,39 @@
+import { LightningElement, api } from "lwc";
+import { OmniscriptBaseMixin } from "omnistudio/omniscriptBaseMixin";
+
+export default class OmniUpperCaseText extends OmniscriptBaseMixin(
+    LightningElement
+) {
+    @api
+    text; // lightning-input text value
+
+    @api
+    label; // lightning-input label
+
+    connectedCallback() {
+        // console.log("OmniScript Json String", this.omniJsonDataStr);
+        console.log("omniJsonDef:", JSON.stringify(this.omniJsonDef));
+
+        // assumes the data is inside of the same custom component
+        const state = this.omniGetSaveState(this.omniJsonDef.name);
+        //console.log("state", JSON.stringify(state));
+        if (state && state.savedText) {
+            this.text = state.savedText;
+        }
+    }
+
+    handleChange(event) {
+        // toUpperCase
+        this.text = event.target.value.toUpperCase();
+        this.omniUpdateDataJson(this.text);
+    }
+
+    disconnectedCallback() {
+        // ref: https://help.salesforce.com/s/articleView?id=sf.os_save_data_inside_custom_component.htm&type=5
+        let mySaveState = { savedText: this.text };
+        // In disconnected callbacks, the usePubSub argument must be set to true.
+        // Events in disconnected callbacks can only be passed using pubsub.
+        let usePubSub = true;
+        this.omniSaveState(mySaveState, this.omniJsonDef.name, usePubSub);
+    }
+}
